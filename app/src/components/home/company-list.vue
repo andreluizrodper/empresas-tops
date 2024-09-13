9d65f2325fb434e62a9ed83d22fef3bbff198cf5
andreluiz <andreluizrodper@gmail.com>
2024-09-13 14:27:06 -0300

    Fix company list

--- app/src/components/home/company-list.vue
+++ app/src/components/home/company-list.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="py-8 flex flex-col gap-8">
     <p class="text-center text-xl">Todas as empresas que já apareceram aqui:</p>
-    <div class="px-4 grid grid-cols-2 md:grid-cols-4 gap-4">
-      <div v-for="item in companies" :key="item.id" class="border rounded">
+    <div class="px-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+      <div
+        v-for="item in companies"
+        :key="item.id"
+        class="border rounded px-4 py-2 flex justify-between items-center"
+      >
         <p class="text-xl font-bold">{{ item.name }}</p>
+        <div class="flex flex-col gap-1 justify-center items-center">
+          <p class="text-sm font-bold">{{ item.amount }}</p>
+          <p class="text-xs">denúncias</p>
+        </div>
       </div>
     </div>
   </div>
@@ -19,7 +27,15 @@ export default {
     };
   },
   async mounted() {
-    this.companies = await getIssue();
+    const companies = await getIssue();
+    const names = [...new Set(companies.map((company) => company.name))];
+    names.forEach((name, index) => {
+      this.companies.push({
+        name,
+        id: index,
+        amount: companies.filter((company) => company.name === name).length,
+      });
+    });
   },
 };
 </script>
